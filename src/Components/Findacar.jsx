def82211ad99b0178be0b45e240a078504988be1
kashif <kashif.shah@mercurialminds.com>
2023-10-25 13:10:11 +0500

    add gogle font and update book a car component

--- src/Components/Findacar.jsx
+++ src/Components/Findacar.jsx
@@ -61,51 +61,52 @@ const Findacar = () => {
     );
   };
 
-  const RenderCars = () => {
-    return cardData.map((car) => (
-      <div className="stock-car-checklist stock-car-checklist-position-relative" key={car.veh_id}>
-        {/* <div className="card-media"> */}
-        <img
-          className="card-media p-2 rounded-top"
-          src={VEH_IMAGE_URL + car.images[0]}
-          alt={`${car.make} not found`}
-        />
-        <span className="specialSpan">SPECIAL</span>
-        {/* </div> */}
-        <div className="card-body">
-          <div className="card-body-wrap">
-            <div className="car-type">{car.veh_name}</div>
-            <div className="car-co">{car.make}</div>
-            <div className="car-des">{car.body_type}</div>
-            <div className="car-rating">
-              <div className="rating-1">Year: {car.year}</div>
-              <div className="condition">Average</div>
-              <div className="reviews">(30 Reviews)</div>
-            </div>
-            <div className="car-des">
-              <div className="fuel-type">
-                <CarOutlined /> &nbsp;{car.milage}
+  const RenderCars1 = ({ car }) => {
+    return (
+      <div className="card bg-whiteSmoke myCarCard col-lg-5 m-4">
+        <div className="row g-0">
+          <div className="col-md-4 d-flex justify-content-center align-items-center">
+            <img
+              src={VEH_IMAGE_URL + car.images[0]}
+              className="img-fluid rounded"
+              alt="image_not_found"
+            />
+          </div>
+          <div className="col-md-8 ">
+            <div className="card-body">
+              <h5 className="card-title">{car.veh_name}</h5>
+              <h6 className="card-title">{car.make}</h6>
+              <p className="card-text">{car.body_type}</p>
+              <div className="car-rating">
+                <div className="rating-1">Year: {car.year}</div>
+                <div className="condition">Average</div>
+                <div className="reviews">(30 Reviews)</div>
               </div>
-              <div className="car-cap d-flex justify-content-center align-items-center">
-                <CalendarOutlined /> &nbsp; {car.seats} Seats
+              <div className="car-des">
+                <div className="fuel-type">
+                  <CarOutlined /> &nbsp;{car.milage}
+                </div>
+                <div className="car-cap d-flex justify-content-center align-items-center">
+                  <CalendarOutlined /> &nbsp; {car.seats} Seats
+                </div>
+                <div className="car-trans">{car.fuel_type}</div>
               </div>
-              <div className="car-trans">{car.fuel_type}</div>
-            </div>
-            <div className="car-price">
-              <div className="price">{car.rental_details.split("/")[0] || car.rent}</div>
-              <div className="more-details">
-                <span
-                  onClick={() => routeToCars(car)}
-                  className="pointer d-flex justify-content-center align-items-center"
-                >
-                  See Details <RightOutlined />
-                </span>
+              <div className="car-price">
+                <div className="price">{car.rental_details.split("/")[0] || car.rent}</div>
+                <div className="more-details">
+                  <span
+                    onClick={() => routeToCars(car)}
+                    className="pointer d-flex justify-content-center align-items-center"
+                  >
+                    See Details <RightOutlined />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    ));
+    );
   };
 
   return (
@@ -127,8 +128,9 @@ const Findacar = () => {
         <div className="stock-cars-main">
           <div className="carsstock-heading">Cars in Stock</div>
           <div className="carstock-sub-heading">Book with confidence:No car booking fees</div>
-          <div className="stock-car-checklist-container">
-            <RenderCars />
+
+          <div className="row d-flex justify-content-between">
+            {cardData.length && cardData.map((car, index) => <RenderCars1 car={car} key={index} />)}
           </div>
         </div>
       </div>
